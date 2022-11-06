8bcc4c068d59ba136963264610067729e026d2e3
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2022-11-06 10:06:30 +0200

    solve 01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
+let intervalId = null;
+
+btnStart.addEventListener('click', onBtnStart);
+btnStop.addEventListener('click', onBtnStop);
+
+function onBtnStart() {
+    btnStart.disabled = true,
+    intervalId = setInterval(() => { changeBGColor() }, 1000)
+}
+
+function onBtnStop() {
+    btnStart.disabled = false,
+    clearInterval(intervalId)
+}
+
+function changeBGColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
