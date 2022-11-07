c9635efd72228b9b314dde865cd55db51256e661
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2022-11-07 22:18:08 +0200

    start 03-promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,3 +6,29 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+const refs = {
+  form: document.querySelector('.form'),
+  btnCreatePromises: document.querySelector('button'),
+}
+
+refs.form.addEventListener('input', onFormInput);
+
+const formData = {};
+
+console.log(refs.form)
+console.log(refs.btnCreatePromises)
+
+function onFormInput(event) {
+  console.log('Show me INPUT');
+  formData[event.target.name] = event.target.value;
+  console.log(formData);
+}
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
