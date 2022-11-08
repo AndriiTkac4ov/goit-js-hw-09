3fdde9e57ef051a5e7a8222fdd3a64008709ca9c
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2022-11-08 15:04:42 +0000

    Deploying to gh-pages from @ AndriiTkac4ov/goit-js-hw-09@475827253cd8bdf209fc80bea376d711dc07d171 🚀

--- 03-promises.4eeaab13.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={form:document.querySelector(".form"),btnCreatePromises:document.querySelector("button")};e.form.addEventListener("input",(function(e){console.log("Show me INPUT"),o[e.target.name]=e.target.value,console.log(o)}));var o={};console.log(e.form),console.log(e.btnCreatePromises)}();
-//# sourceMappingURL=03-promises.4eeaab13.js.map
--- 03-promises.f475f8c7.js
+++
@@ -1,2 +0,0 @@
-const o={form:document.querySelector(".form"),btnCreatePromises:document.querySelector("button")};o.form.addEventListener("input",(function(o){console.log("Show me INPUT"),e[o.target.name]=o.target.value,console.log(e)}));const e={};console.log(o.form),console.log(o.btnCreatePromises);
-//# sourceMappingURL=03-promises.f475f8c7.js.map
