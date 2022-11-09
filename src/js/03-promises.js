681066186b1e606eab7f3b69bf0f22de22b6bf65
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2022-11-09 02:00:49 +0200

    finish 03-promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,24 +1,24 @@
+// 1)
 import Notiflix from 'notiflix';
 
-const refs = {
-  form: document.querySelector('.form'),
-  btnCreatePromises: document.querySelector('button'),
-}
+const formOfGenerator = document.querySelector('.form');
 
-refs.form.addEventListener('input', createDatesForPromisesGenerator);
-refs.btnCreatePromises.addEventListener('click', startPromisesGenerator);
+formOfGenerator.addEventListener('submit', onFormSubmit);
 
-function startPromisesGenerator() {
-  console.log('You are on the right way;) keep going!!!');
-  createPromise(12, 2000).then(onMakeNotifySuccess).catch(onMakeNotifyError);
-}
+function onFormSubmit (event) {
+  event.preventDefault();
 
-const formData = {};
+  const formData = event.currentTarget.elements;
 
-function createDatesForPromisesGenerator(event) {
-  formData[event.target.name] = event.target.value;
-  console.log(formData);
-  // createPromise(position, delay);
+  const firstDelay = formData.delay.value;
+  const delayStep = formData.step.value;
+  const amount = formData.amount.value;
+
+  for (let index = 1; index <= Number(amount); index += 1) {
+    const mainDelay = Number(firstDelay) + Number(delayStep) * (index - 1);
+    
+    createPromise(index, mainDelay).then(onMakeNotifySuccess).catch(onMakeNotifyError);
+  };
 }
 
 function createPromise(position, delay) {
@@ -27,9 +27,9 @@ function createPromise(position, delay) {
 
     setTimeout(() => {
       if (shouldResolve) {
-        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`)
+        resolve({ position, delay })
       } else {
-        reject(`❌ Rejected promise ${position} in ${delay}ms`)
+        reject({ position, delay })
       }
     }, delay);
   })
@@ -37,10 +37,92 @@ function createPromise(position, delay) {
   return promise;
 }
 
-function onMakeNotifySuccess(result) {
-  Notiflix.Notify.success(result);
+function onMakeNotifySuccess({ position, delay }) {
+  Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
 }
 
-function onMakeNotifyError(result) {
-  Notiflix.Notify.failure(result);
+function onMakeNotifyError({ position, delay }) {
+  Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ========================================================================
+
+// 2)
+// import Notiflix from 'notiflix';
+
+// const refs = {
+//   form: document.querySelector('.form'),
+//   btnCreatePromises: document.querySelector('button'),
+// }
+
+// refs.form.addEventListener('input', gatherDatesForPromisesGenerator);
+// refs.btnCreatePromises.addEventListener('click', startPromisesGenerator);
+
+// const formData = {};
+
+// function startPromisesGenerator(event) {
+//   event.preventDefault();
+
+//   gatherDatesForPromisesGenerator(event);
+
+//   for (let index = 1; index <= Number(amount); index += 1) {
+//     const mainDelay = Number(delay) + Number(step) * (index - 1);
+    
+//     createPromise(index, mainDelay).then(onMakeNotifySuccess).catch(onMakeNotifyError);
+//   };
+// }
+
+// function gatherDatesForPromisesGenerator(event) {
+//   formData[event.target.name] = event.target.value;
+  
+//   return { delay, step, amount } = formData;
+// }
+
+// function createPromise(position, delay) {
+//   const promise = new Promise((resolve, reject) => {
+//     const shouldResolve = Math.random() > 0.3;
+
+//     setTimeout(() => {
+//       if (shouldResolve) {
+//         resolve({ position, delay })
+//       } else {
+//         reject({ position, delay })
+//       }
+//     }, delay);
+//   })
+
+//   return promise;
+// }
+
+// function onMakeNotifySuccess({ position, delay }) {
+//   Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+// }
+
+// function onMakeNotifyError({ position, delay }) {
+//   Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+// }
